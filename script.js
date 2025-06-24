bd37e45ff716272b1eb4df0afe3b6a435d5a3a88
Sleni64 <Sarabellahleni@gmail.com>
2025-06-24 14:39:35 -0700

    testing second slide show

--- script.js
+++ script.js
@@ -7,6 +7,25 @@ const mydrinks = ["projecttea", "mondoscoffeehouse", "thebobastop", "dutchbros",
 const myfastfood = ["wingstop", "tacobell", "canes", "jerseymikes", "pandaexpress"]
 
 
+let slideIndex = 0;
+showSlides();
+
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+
+
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); //change image every 2 seconds
+}
+
+
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -23,15 +42,15 @@ function openTab(evt, tabName) {
 
 
 //random local
-document.getElementById("randomlocal").innerHTML = mylocal[Math.floor(Math.random() * mylocal.length)];
+//document.getElementById("randomlocal").innerHTML=mylocal[Math.floor(Math.random()*mylocal.length)];
 
 
 //random drinks
-document.getElementById("randomdrinks").innerHTML = mydrinks[Math.floor(Math.random() * mydrinks.length)];
+//document.getElementById("randomdrinks").innerHTML=mydrinks[Math.floor(Math.random()*mydrinks.length)];
 
 
 //random chiain
-document.getElementById("randomfastfood").innerHTML = myfastfood[Math.floor(Math.random() * myfastfood)];
+//document.getElementById("randomfastfood").innerHTML=myfastfood[Math.floor(Math.random()*myfastfood)];
 
 
 
@@ -41,25 +60,7 @@ function scrollToSection(sectionId) {
     section.scrollIntoView({ behavior: "smooth" });
 }
 
-let slideIndex = 0;
-showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 3 seconds
-}
 
 
 
