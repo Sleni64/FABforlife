4b588cf409670335a5e359c2da774d39ec24faed
Sleni64 <Sarabellahleni@gmail.com>
2025-06-25 10:55:24 -0700

    animate title

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ function scrollToSection(sectionId) {
 
 
 
-
+//animate booms
 anime.timeline({loop: true})
   .add({
     targets: '.ml15 .word',
@@ -77,4 +77,26 @@ anime.timeline({loop: true})
     duration: 1000,
     easing: "easeOutExpo",
     delay: 1000
+  });
+
+  // animate the title
+var textWrapper = document.querySelector('.ml12');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml12 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  }).add({
+    targets: '.ml12 .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
   });
