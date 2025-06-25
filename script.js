732c18c91301c4a003338de780f8a608d386c218
Sleni64 <Sarabellahleni@gmail.com>
2025-06-25 10:22:25 -0700

    animate booms

--- script.js
+++ script.js
@@ -54,7 +54,6 @@ function openTab(evt, tabName) {
 
 
 
-
 function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     section.scrollIntoView({ behavior: "smooth" });
@@ -64,3 +63,18 @@ function scrollToSection(sectionId) {
 
 
 
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml15 .word',
+    scale: [14,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+    duration: 800,
+    delay: (el, i) => 800 * i
+  }).add({
+    targets: '.ml15',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
