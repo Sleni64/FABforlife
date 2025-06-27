06a70cfa79b1e6984e9c1b5243871810fe419ffd
Sleni64 <Sarabellahleni@gmail.com>
2025-06-27 08:32:07 -0700

    final updates

--- script.js
+++ script.js
@@ -2,53 +2,81 @@ const mylocal = ["bstreet", "acataco", "celestinos", "unwindkitchenandbar", "str
 
 
 
+
+
+
+
+
 const mydrinks = ["projecttea", "mondoscoffeehouse", "thebobastop", "dutchbros", "starbucks"]
+
+
+
+
+
 
 
 
 const myfastfood = ["wingstop", "tacobell", "canes", "jerseymikes", "pandaexpress"]
 
 
+
+
+
+
+
+
 let slideIndex = 0;
 showSlides();
 
 
+
+
+
+
+
+
 function showSlides() {
-   let i;
-   let slides = document.getElementsByClassName("mySlides");
-   for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";
-   }
-   slideIndex++;
-   if (slideIndex > slides.length) { slideIndex = 1 }
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
 
 
-   slides[slideIndex - 1].style.display = "block";
-   setTimeout(showSlides, 3000); //change image every 2 seconds
+
+
+
+
+
+
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); //change image every 2 seconds
 }
 
 
 thingtoappear = document.getElementById("appearme");
-thingtoappear.style.display="none";
+thingtoappear.style.display = "none";
 
 
 function openTab(evt, tabName) {
-   var i, tabcontent, tablinks;
-   thingtohide = document.getElementById("hideme");
-   thingtohide.style.display="none";
-   thingtoappear.style.display="block"
+    var i, tabcontent, tablinks;
+    thingtohide = document.getElementById("hideme");
+    thingtohide.style.display = "none";
+    thingtoappear.style.display = "block"
 
 
-   tabcontent = document.getElementsByClassName("tabcontent");
-   for (i = 0; i < tabcontent.length; i++) {
-       tabcontent[i].style.display = "none";
-   }
-   tablinks = document.getElementsByClassName("tablinks");
-   for (i = 0; i < tablinks.length; i++) {
-       tablinks[i].className = tablinks[i].className.replace(" active", "");
-   }
-   document.getElementById(tabName).style.display = "block";
-   evt.currentTarget.className += "active";
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += "active";
 }
 
 
@@ -72,103 +100,588 @@ function openTab(evt, tabName) {
 //document.getElementById("randomdrinks").innerHTML=mydrinks[Math.floor(Math.random()*mydrinks.length)];
 
 
+
+
+
+
+
+
 //random chiain
 //document.getElementById("randomfastfood").innerHTML=myfastfood[Math.floor(Math.random()*myfastfood)];
 
 
+
+
+
+
+
+
+
+
+
+
 function scrollToSection(sectionId) {
-   const section = document.getElementById(sectionId);
-   section.scrollIntoView({ behavior: "smooth" });
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: "smooth" });
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //animate booms
-anime.timeline({loop: true})
- .add({
-   targets: '.ml15 .word',
-   scale: [14,1],
-   opacity: [0,1],
-   easing: "easeOutCirc",
-   duration: 800,
-   delay: (el, i) => 300 * i
- }).add({
-   targets: '.ml15',
-   opacity: 0,
-   duration: 500,
-   easing: "easeOutExpo",
-   delay: 1000
- });
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml15 .word',
+        scale: [14, 1],
+        opacity: [0, 1],
+        easing: "easeOutCirc",
+        duration: 800,
+        delay: (el, i) => 300 * i
+    }).add({
+        targets: '.ml15',
+        opacity: 0,
+        duration: 500,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
 
 
 
 
- // animate the title
+// animate the title
 var textWrapper = document.querySelector('.ml12');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 
 
 
-anime.timeline({loop: true})
- .add({
-   targets: '.ml12 .letter',
-   translateX: [40,0],
-   translateZ: 0,
-   opacity: [0,1],
-   easing: "easeOutExpo",
-   duration: 1200,
-   delay: (el, i) => 500 + 30 * i
- }).add({
-   targets: '.ml12 .letter',
-   translateX: [0,-30],
-   opacity: [1,0],
-   easing: "easeInExpo",
-   duration: 1100,
-   delay: (el, i) => 100 + 30 * i
- });
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml12 .letter',
+        translateX: [40, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 1200,
+        delay: (el, i) => 500 + 30 * i
+    }).add({
+        targets: '.ml12 .letter',
+        translateX: [0, -30],
+        opacity: [1, 0],
+        easing: "easeInExpo",
+        duration: 1100,
+        delay: (el, i) => 100 + 30 * i
+    });
+
+
+
+
+
+
+
+
 
 
 var textWrapper = document.querySelector('.test-animation');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 
-anime.timeline({loop: true})
- .add({
-   targets: '.test-animation .letter',
-   translateX: [40,0],
-   translateZ: 0,
-   opacity: [0,1],
-   easing: "easeOutExpo",
-   duration: 1200,
-   delay: (el, i) => 500 + 30 * i
- }).add({
-   targets: '.test-animation .letter',
-   translateX: [0,-30],
-   opacity: [1,0],
-   easing: "easeInExpo",
-   duration: 1100,
-   delay: (el, i) => 100 + 30 * i
- });
 
 
- var textWrapper = document.querySelector('.test-animation');
+
+
+
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.test-animation .letter',
+        translateX: [40, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 1200,
+        delay: (el, i) => 500 + 30 * i
+    }).add({
+        targets: '.test-animation .letter',
+        translateX: [0, -30],
+        opacity: [1, 0],
+        easing: "easeInExpo",
+        duration: 1100,
+        delay: (el, i) => 100 + 30 * i
+    });
+
+
+
+
+
+
+var textWrapper = document.querySelector('.test-animation');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 
-anime.timeline({loop: true})
-  .add({
-    targets: '.test-animation .letter',
-    translateX: [40,0],
-    translateZ: 0,
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 1200,
-    delay: (el, i) => 500 + 30 * i
-  }).add({
-    targets: '.test-animation .letter',
-    translateX: [0,-30],
-    opacity: [1,0],
-    easing: "easeInExpo",
-    duration: 1100,
-    delay: (el, i) => 100 + 30 * i
-  });
+
+
+
+
+
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.test-animation .letter',
+        translateX: [40, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 1200,
+        delay: (el, i) => 500 + 30 * i
+    }).add({
+        targets: '.test-animation .letter',
+        translateX: [0, -30],
+        opacity: [1, 0],
+        easing: "easeInExpo",
+        duration: 1100,
+        delay: (el, i) => 100 + 30 * i
+    });
+
+
+
+
+
+
+
+
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+
+
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml6 .letter',
+        translateY: ["1.1em", 0],
+        translateZ: 0,
+        duration: 750,
+        delay: (el, i) => 50 * i
+    }).add({
+        targets: '.ml6',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml5 .line',
+        opacity: [0.5, 1],
+        scaleX: [0, 2],
+        easing: "easeInOutExpo",
+        duration: 700
+    }).add({
+        targets: '.ml5 .line',
+        duration: 600,
+        easing: "easeOutExpo",
+        translateY: (el, i) => (-0.625 + 0.625 * 2 * i) + "em"
+    }).add({
+        targets: '.ml5 .ampersand',
+        opacity: [0, 1],
+        scaleY: [0.5, 2],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: '-=600'
+    }).add({
+        targets: '.ml5 .letters-left',
+        opacity: [0, 1],
+        translateX: ["3em", 0],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: '-=100'
+    }).add({
+        targets: '.ml5 .letters-right',
+        opacity: [0, 1],
+        translateX: ["-3em", 0],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: '-=800'
+    }).add({
+        targets: '.ml5',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
+
+
+
+
+
+
+
+
+var textWrapper = document.querySelector('.ml7 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+
+
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml7 .letter',
+        translateY: ["1.1em", 0],
+        translateX: ["0.55em", 0],
+        translateZ: 0,
+        rotateZ: [180, 0],
+        duration: 750,
+        easing: "easeOutExpo",
+        delay: (el, i) => 50 * i
+    }).add({
+        targets: '.ml7',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+
+
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml11 .line',
+        scaleY: [0, 1],
+        opacity: [0.5, 1],
+        easing: "easeOutExpo",
+        duration: 700
+    })
+    .add({
+        targets: '.ml11 .line',
+        translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+        easing: "easeOutExpo",
+        duration: 700,
+        delay: 100
+    }).add({
+        targets: '.ml11 .letter',
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: '-=775',
+        delay: (el, i) => 34 * (i + 1)
+    }).add({
+        targets: '.ml11',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+var textWrapper = document.querySelector('.ml12');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+
+
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml12 .letter',
+        translateX: [40, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 1200,
+        delay: (el, i) => 500 + 30 * i
+    }).add({
+        targets: '.ml12 .letter',
+        translateX: [0, -30],
+        opacity: [1, 0],
+        easing: "easeInExpo",
+        duration: 1100,
+        delay: (el, i) => 100 + 30 * i
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+aanime.timeline({ loop: true })
+    .add({
+        targets: '.ml155 .word',
+        scale: [14, 1],
+        opacity: [0, 1],
+        easing: "easeOutCirc",
+        duration: 800,
+        delay: (el, i) => 800 * i
+    }).add({
+        targets: '.ml155',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var textWrapper = document.querySelector('.ml10 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+
+
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml10 .letter',
+        rotateY: [-90, 0],
+        duration: 1300,
+        delay: (el, i) => 45 * i
+    }).add({
+        targets: '.ml10',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
+
+
+
+
+
+
+
+
+var textWrapper = document.querySelector('.ml1 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+
+
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml1 .letter',
+        scale: [0.3, 1],
+        opacity: [0, 1],
+        translateZ: 0,
+        easing: "easeOutExpo",
+        duration: 600,
+        delay: (el, i) => 70 * (i + 1)
+    }).add({
+        targets: '.ml1 .line',
+        scaleX: [0, 1],
+        opacity: [0.5, 1],
+        easing: "easeOutExpo",
+        duration: 700,
+        offset: '-=875',
+        delay: (el, i, l) => 80 * (l - i)
+    }).add({
+        targets: '.ml1',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
+
+
+
+
+
+
+
+
+var textWrapper = document.querySelector('.ml9 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+
+
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml9 .letter',
+        scale: [0, 1],
+        duration: 1500,
+        elasticity: 600,
+        delay: (el, i) => 45 * (i + 1)
+    }).add({
+        targets: '.ml9',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
+
+
+
+
+
+
+
+
+var ml4 = {};
+ml4.opacityIn = [0, 1];
+ml4.scaleIn = [0.2, 1];
+ml4.scaleOut = 3;
+ml4.durationIn = 800;
+ml4.durationOut = 600;
+ml4.delay = 500;
+
+
+
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml4 .letters-1',
+        opacity: ml4.opacityIn,
+        scale: ml4.scaleIn,
+        duration: ml4.durationIn
+    }).add({
+        targets: '.ml4 .letters-1',
+        opacity: 0,
+        scale: ml4.scaleOut,
+        duration: ml4.durationOut,
+        easing: "easeInExpo",
+        delay: ml4.delay
+    }).add({
+        targets: '.ml4 .letters-2',
+        opacity: ml4.opacityIn,
+        scale: ml4.scaleIn,
+        duration: ml4.durationIn
+    }).add({
+        targets: '.ml4 .letters-2',
+        opacity: 0,
+        scale: ml4.scaleOut,
+        duration: ml4.durationOut,
+        easing: "easeInExpo",
+        delay: ml4.delay
+    }).add({
+        targets: '.ml4 .letters-3',
+        opacity: ml4.opacityIn,
+        scale: ml4.scaleIn,
+        duration: ml4.durationIn
+    }).add({
+        targets: '.ml4 .letters-3',
+        opacity: 0,
+        scale: ml4.scaleOut,
+        duration: ml4.durationOut,
+        easing: "easeInExpo",
+        delay: ml4.delay
+    }).add({
+        targets: '.ml4',
+        opacity: 0,
+        duration: 500,
+        delay: 500
+    });
+
+
+
+
+
+
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml2 .letter',
+        scale: [4, 1],
+        opacity: [0, 1],
+        translateZ: 0,
+        easing: "easeOutExpo",
+        duration: 950,
+        delay: (el, i) => 70 * i
+    }).add({
+        targets: '.ml2',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
+
+
+
+
+
+
+
+
+
